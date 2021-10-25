6085fd7c24011b9057ce209528c121e7c83c00b6
solidmaradini <solidmaradini@gmail.com>
2021-10-25 11:04:35 +0200

    first exercise complete

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let listaCompra =["limpiahorno", "pimienta", "salmon ahumado", "chocolate", "velas"];
+
+
+
+function activacion(){
+        for (let index = 0; index < listaCompra.length; index++) {
+                console.log(listaCompra[index])
+    
+    }
+}
+activacion();
+
+
+
